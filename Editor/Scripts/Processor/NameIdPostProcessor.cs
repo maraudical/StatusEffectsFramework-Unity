1ae90059114707f106724fe52518e801cdb0d387
Grady <gradymilligan@icloud.com>
2024-12-21 12:38:48 -0800

    name ids reset fix when loading editor

--- Editor/Scripts/Processor/NameIdPostProcessor.cs
+++ Editor/Scripts/Processor/NameIdPostProcessor.cs
@@ -1,9 +1,15 @@
+using System.Collections.Generic;
 using UnityEditor;
 
 namespace StatusEffects.Inspector
 {
     public class NameIdPostProcessor : AssetPostprocessor
     {
+#if UNITY_EDITOR
+        private static Dictionary<string, Name> s_NameIds;
+#endif
+        private static Name m_Reference;
+
         static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
         {
             int importedCount = importedAssets.Length;
@@ -13,7 +19,25 @@ namespace StatusEffects.Inspector
                 UnityEngine.Object asset = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(assetPath);
                 
                 if (asset != null && asset is Name name)
-                    name.GenerateId();
+                {
+                    if (s_NameIds == null)
+                        s_NameIds = new Dictionary<string, Name>();
+
+                    if (!string.IsNullOrWhiteSpace(name.Id))
+                        if (s_NameIds.TryGetValue(name.Id, out m_Reference) && m_Reference != name)
+                            GenerateUntilAddable();
+                        else
+                            s_NameIds.Add(name.Id, name);
+                    else
+                        GenerateUntilAddable();
+
+                    void GenerateUntilAddable()
+                    {
+                        name.GenerateId();
+                        if (!s_NameIds.TryAdd(name.Id, name)
+)                           GenerateUntilAddable();
+                    }
+                }
             }
         }
     }
