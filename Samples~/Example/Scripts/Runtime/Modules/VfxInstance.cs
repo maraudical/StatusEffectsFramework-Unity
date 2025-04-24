b385438db660019862120312f41630235e0b9c06
Maraudical <gradymilligan@icloud.com>
2025-04-24 15:44:06 -0700

    script name fix

---
+++ Samples~/Example/Scripts/Runtime/Modules/VfxInstance.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace StatusEffects.Modules
+{
+    public class VfxInstance : ModuleInstance
+    {
+        public GameObject Prefab;
+        public bool InstantiateAgainWhenAddingStacks;
+    }
+}
